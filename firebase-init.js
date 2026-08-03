5f4ad2904d3466687050005ead5f5c0e3beca8d3
GabrielC <gabrielc.adorno.22@gmail.com>
2026-08-03 19:52:54 -0300

    Cadastro de usuario

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyChU_6JRVmz6lunbmWPEKlg9tOkvi9gTtg",
+    authDomain: "controle-de-gastos-b4262.firebaseapp.com",
+    projectId: "controle-de-gastos-b4262",
+    storageBucket: "controle-de-gastos-b4262.firebasestorage.app",
+    messagingSenderId: "34657504345",
+    appId: "1:34657504345:web:fc7ed61d2f6e8f45e13548"
+  };
+  firebase.initializeApp(firebaseConfig);
